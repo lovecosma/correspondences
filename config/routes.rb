228e6e34c83cd65e0d4bf0b17875860acd3c2ae4
Matthew Williams <williamsmatthew333@gmail.com>
2022-01-15 20:45:18 -0700

    changed order of routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  root to: "application#welcome"
   resources :intentions
   resources :correspondences
   resources :categories
+  root to: "application#welcome"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
